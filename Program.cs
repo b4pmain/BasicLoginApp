8f44f0e111b403832e48408c1d7b2f5fdcff84d5
Edriansenar <estarhymar@gmail.com>
2026-02-14 17:20:30 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace BasicLoginApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "Master";
+            string password = "Password123";
+
+            Console.WriteLine("Enter your Username: ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("Enter your Password");
+            string passInput = Console.ReadLine();
+
+            if (userInput == username && passInput == password)
+            {
+                Console.WriteLine("Login Successful!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Credentials.");
+            }
+        }
+    }
+}
